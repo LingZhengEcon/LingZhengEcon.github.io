d726dadca551921329e21045b737864d116c66d4
Ling Zheng <acousticzheng@gmail.com>
2026-07-13 11:33:26 +0800

    Stitch homepage speech bubble tail

--- assets/js/home.js
+++ assets/js/home.js
@@ -11,11 +11,24 @@
 
   const idea = ideas[Math.floor(Math.random() * ideas.length)];
   const message = document.createElement(idea.url ? "a" : "span");
+  const outline = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const outlinePath = document.createElementNS("http://www.w3.org/2000/svg", "path");
 
   message.textContent = idea.text;
   if (idea.url) message.href = idea.url;
 
-  bubble.replaceChildren(message);
+  outline.setAttribute("class", "home-speech-outline");
+  outline.setAttribute("viewBox", "0 0 300 154");
+  outline.setAttribute("preserveAspectRatio", "none");
+  outline.setAttribute("aria-hidden", "true");
+  outline.setAttribute("focusable", "false");
+  outlinePath.setAttribute(
+    "d",
+    "M64 121 L55 151 L88 128 C119 138 176 140 223 130 C273 119 299 94 290 63 C278 22 222 7 151 8 C78 9 27 28 14 66 C3 98 25 118 64 121 Z"
+  );
+  outline.append(outlinePath);
+
+  bubble.replaceChildren(outline, message);
   bubble.hidden = false;
 
   window.requestAnimationFrame(() => {
